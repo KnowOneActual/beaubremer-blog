9418903481ee66940adb7c364cef720fb178db2a
Beau Bremer <beau.bremer@gmail.com>
2025-06-11 16:22:07 -0500

    Updated style and added 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,14 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob("posts/*.md");
   });
 
+  // This new line ensures the 'img' folder and its contents are copied
+  eleventyConfig.addPassthroughCopy("img");
+
   return {
+    // This tells Eleventy to use Nunjucks for all Markdown files
     markdownTemplateEngine: "njk",
+    
+    // This defines the directory structure
     dir: {
       input: ".", // Use the root as the input directory
       output: "_site",
